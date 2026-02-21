1f5c8faf216fae99c8468905b34eae557e6958f9
zainab7804 <123864934+zainab7804@users.noreply.github.com>
2026-02-21 16:50:34 +0530

    Create Turning Left.py

---
+++ Motion Control/Turning Left.py
@@ -0,0 +1,14 @@
+#TURNING LEFT
+from controller import Robot
+#imports robot library
+robot=Robot()
+TIME_STEP=int(robot.getBasicTimeStep())
+MAX_SPEED=6.20
+left_motor=robot.getMotor("motor_1")
+right_motor=robot.getMotor("motor_2")
+left_motor.setPosition(float('inf'))
+#setting position to infinity is called velocity control
+right_motor.setPosition(float('inf'))
+while robot.step(TIME_STEP)!=-1:
+ left_motor.setVelocity(0.3*MAX_SPEED)
+ right_motor.setVelocity(MAX_SPEED)
